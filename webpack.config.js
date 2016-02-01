aee20f50171e969da882960108476223778a5a42
Hunter Husar <hunter@hunterhusar.net>
2016-01-31 21:45:22 -0600

    server.js build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var webpack = require('webpack');
+var path = require('path');
 
 var getEntry = function(env) {
   var entry = [];
@@ -6,26 +7,43 @@ var getEntry = function(env) {
     // hot reloading only in dev
     entry.push('webpack-hot-middleware/client');
   }
-  entry.push('./entry.js');
+  entry.push('./src/index');
   return entry;
 }
 
-// webpack ./entry.js bundle.js --module-bind 'css=style!css'
-module.exports = {
-    entry: getEntry("development"),
+var getLoaders = function () {
+  return [
+    {
+      test: /\.js$/,
+      include: path.join(__dirname, 'src'),
+      loaders: ['babel', 'eslint']
+    },
+    {
+      test: /(\.css|\.scss)$/,
+      include: path.join(__dirname, 'src'),
+      loaders: ['style', 'css?sourceMap', 'sass?sourcMap']
+    }
+  ]
+};
+
+module.exports = function getConfig(env) {
+  env = "development"
+  return {
+    debug: true,
+    devtool: env == 'production' ? 'source-map' : 'cheap-module-eval-source-map',
+    entry: getEntry(env),
     output: {
         path: __dirname + '/dist',
         publicPath: '',
         filename: "bundle.js"
     },
-    module: {
-        loaders: [
-            { test: /\.css$/, loader: "style!css" }
-        ]
-    },
     plugins: [
         new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoErrorsPlugin()
-    ]
+    ],
+    module: {
+      loaders: getLoaders()
+    }
+  };
 };
