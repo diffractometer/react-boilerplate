c8b50fd44f8f6440a30e2952111013dde6bfc24d
Hunter Husar <hunter@hunterhusar.net>
2016-01-31 23:37:35 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-//var msg = "foo bar zod";
-//console.log(msg);
-//import './styles/styles.scss';
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+
+import './styles/styles.scss';
+
+class App extends React.component {
+  render() {
+    return (
+      <h1>React!</h1>
+    )
+  }
+}
+
+render(
+  <h1>yo</h1>
+)
