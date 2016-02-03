fb1edf93bfd4aaff13cada14f46ae6df5e241bcb
Hunter Husar <hunter@hunterhusar.net>
2016-02-02 18:21:13 -0600

    ie8

--- webpack.config.js
+++ webpack.config.js
@@ -29,12 +29,15 @@ var getPlugins = function(env) {
 var getEntry = function(env) {
   var entry = [];
   if (env=='development') {
+    // IE polyfill's
+    entry.push('aight');
+    entry.push('event-source-polyfill');
     // hot reloading only in dev
     entry.push('webpack-hot-middleware/client');
   }
   entry.push('./src/index');
   return entry;
-}
+};
 
 var getLoaders = function () {
   return [
@@ -48,7 +51,7 @@ var getLoaders = function () {
       include: path.join(__dirname, 'src'),
       loaders: ['style', 'css?sourceMap', 'sass?sourcMap']
     }
-  ]
+  ];
 };
 
 module.exports = function getConfig(env) {
